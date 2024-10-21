b5a981b83a5653ed43c085277d30d394dfdffb62
abenavides01 <andrepaobena@gmail.com>
2024-10-21 15:36:06 -0600

    añadiendo cambios en el index

--- My_Trees/index.php
+++ My_Trees/index.php
@@ -2,7 +2,6 @@
 include('utils/functions.php');
 $error_msg = isset($_GET['error']) ? $_GET['error'] : '';
 ?>
-<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -20,10 +19,18 @@ $error_msg = isset($_GET['error']) ? $_GET['error'] : '';
             <p class="lead">User Login</p>
             <hr class="my-4">
         </div>
+        <div class="error">
+            <?php 
+            if (isset($_GET['error'])) {
+                if ($_GET['error'] == 'empty_fields') {
+                    echo "Por favor, completa todos los campos.";
+                } elseif ($_GET['error'] == 'login') {
+                    echo "Credenciales incorrectas. Intenta nuevamente.";
+                }
+            }
+            ?>
+        </div>
         <form method="post" action="actions/login.php">
-            <div class="error">
-                <?php echo $error_msg; ?>
-            </div>
             <div class="form-group">
                 <label for="email">Email Address</label>
                 <input id="email" class="form-control" type="text" name="username">
