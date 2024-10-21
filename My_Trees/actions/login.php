f1d17e9f15868cfda061032d4cebe3f340536390
abenavides01 <andrepaobena@gmail.com>
2024-10-21 15:40:06 -0600

    añadiendo validación de los inputs en el login

--- My_Trees/actions/login.php
+++ My_Trees/actions/login.php
@@ -1,9 +1,22 @@
 <?php
 require('../utils/functions.php');
 
+function validaLoginInput($username, $password) {
+    if (empty($username) || empty($password)) {
+        return false; // Si alguno está vacío, devuelve falso
+    }
+    return true; // Ambos están completos
+}
+
 if ($_POST) {
     $username = $_REQUEST['username'];
     $password = $_REQUEST['password'];
+
+    // Llama a la función de validación
+    if (!validaLoginInput($username, $password)) {
+        header('Location: index.php?error=empty_fields');
+        exit();
+    }
 
     $user = authenticate($username, $password);
 
